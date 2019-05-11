de92633ce62cf60554fe8fde3f4b21167f97d3c0
Dhanush4444 <dhanushdazz@gmail.com>
2019-05-11 10:31:12 +0530

    Updated

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 ## IOT JOKE PI
 Control your devices from [IOT-JOKE](https://iot-joker.firebaseapp.com) by RasperryPi
 
-## Install Requests
-   `pip install requests`
-
 ## Install IOT-JOKE-PI
   `pip install pyiotjokepi --user`
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = "0.2.1"
+VERSION = "0.2.2"
 
 with open('README.rst', 'r') as f:
     long_description = f.read()
@@ -12,9 +12,11 @@ setup(
     long_description=long_description,
     description='',
     author='Dhanush HL',
+    license = 'MIT',
     url='https://github.com/dazzHere/IOT-JOKE-PI',
     author_email='dhanushdazz@gmail.com',
     keywords=['iot', 'iot-joke-pi', 'iot-joke', 'iot-joker'],
+    install_requires=['requests'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
